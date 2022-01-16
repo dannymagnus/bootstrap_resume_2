660f76eba82c98f8153623be50c11b8039ae747d
dannymagnus <mrdanrichards@gmail.com>
2022-01-16 19:30:12 +0000

    add githib text field function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,19 +1,18 @@
 function userInformationHTML(user) {
     return `
         <h2>${user.name}
-            <span class = 'small-name'>
-                (@<a href = ${user.html_url} target = "blank">${user.login}</a>)
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
-        <div class = 'gh-content>
-            <div class = "gh-avatar">
-                <a href = ${user.html_url} target = "blank">
-                    <img src = ${user.avatar_url} width = "80" height = "80" alt = ${user.login}" />
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following: ${user.following} <br>Repos: ${user.public_repos}</p>
-        </div>
-        `;       
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -32,17 +31,17 @@ function fetchGitHubInformation(event) {
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
     ).then(
-        function (response) {
+        function(response) {
             var userData = response;
-            console.log(userData);
-            $('gh-user-data').html(userInformationHTML(userData));
+            $("#gh-user-data").html(userInformationHTML(userData));
         },
-        function (errorResponse) {
+        function(errorResponse) {
             if (errorResponse.status === 404) {
-                $('#gh-user-data').html(`<h2>No info found for ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
-                $('#gh-user-data').html(
+                $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
